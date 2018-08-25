65db0e0a907fd9191908cf455cca637b356f388a
Kayla Wilson <kaylawilson@Kaylas-MacBook-Air-2.local>
2018-08-24 21:22:44 -0400

    Deleted files, uplodated Hangman Files

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,83 +1,235 @@
-var letters = ["a", "e", "i", "o", "u"];
+window.onload = function () {
 
-// This array will hold what we guess
-var guessedLetters = [];
+  var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
+        'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
+        't', 'u', 'v', 'w', 'x', 'y', 'z'];
+  
+  var categories;         // Array of topics
+  var chosenCategory;     // Selected catagory
+  var getHint ;          // Word getHint
+  var word ;              // Selected word
+  var guess ;             // Guess
+  var guesses = [ ];      // Stored guesses
+  var lives ;             // Lives
+  var counter ;           // Count correct guesses
+  var space;              // Number of spaces in word '-'
 
-// This variable will be randomly assigned one of the three letters
-var letterToGuess = null;
-
-// This is what we'll use to count down
-var guessesLeft = 9;
-
-// This is the counter for wins/losses
-var wins = 0;
-var losses = 0;
-
-// Below we created three functions to updateGuesses, updateGuessesLeft, and updateGuessesSoFar
-var updateGuessesLeft = function() {
-  // Here we are grabbing the HTML element and setting it equal to the guessesLeft.
-  // (i.e. guessesLeft will get displayed in HTML)
-  document.querySelector("#guesses-left").innerHTML = guessesLeft;
-};
-
-var updateLetterToGuess = function() {
-  // Here we get a random letterToGuess and assign it based on a random generator (only looking at a, b, or c)
-  letterToGuess = letters[Math.floor(Math.random() * letters.length)];
-};
-
-var updateGuessesSoFar = function() {
-  // Here we take the guesses the user has tried -- then display it as letters separated by commas.
-  document.querySelector("#guesses-so-far").innerHTML = guessedLetters.join(", ");
-};
-
-// Function will be called when we reset everything
-var reset = function() {
-  guessesLeft = 9;
-  guessedLetters = [];
-  updateLetterToGuess();
-  updateGuessesLeft();
-  updateGuessesSoFar();
-};
-
-// Execute on page load.
-updateLetterToGuess();
-updateGuessesLeft();
-
-// This function will capture the keyboard clicks.
-document.onkeydown = function(event) {
-  // It's going to reduce the guesses by one
-  guessesLeft--;
-
-  // Lowercase the letter
-  var letter = String.fromCharCode(event.which).toLowerCase();
-
-  // Then add the guess to the guessedLetters array
-  guessedLetters.push(letter);
-
-  // Then its going to run the update functions
-  updateGuessesLeft();
-  updateGuessesSoFar();
+  // Get elements
+  var showLives = document.getElementById("mylives");
+  var showCatagory = document.getElementById("scatagory");
+  var getHint = document.getElementById("hint");
+  var showClue = document.getElementById("clue");
 
 
-  // We'll check if there's a match.
-  if (letter === letterToGuess) {
 
-    // If there is then we win and we'll update the HTML to display the win.
-    wins++;
-    document.querySelector("#wins").innerHTML = wins;
+  // Allows users to pick alphabets on the html
+  var buttons = function () {
+    myButtons = document.getElementById('buttons');
+    letters = document.createElement('ul');
 
-    // Then we'll reset the game
-    reset();
+    for (var i = 0; i < alphabet.length; i++) {
+      letters.id = 'alphabet';
+      list = document.createElement('li');
+      list.id = 'letter';
+      list.innerHTML = alphabet[i];
+      check();
+      myButtons.appendChild(letters);
+      letters.appendChild(list);
+    }
+  }
+    
+  
+  // Select Catagory
+  var selectCat = function () {
+    if (chosenCategory === categories[0]) {
+      catagoryName.innerHTML = "The Chosen Category Is Different World";
+    } else if (chosenCategory === categories[1]) {
+      catagoryName.innerHTML = "The Chosen Category Is American Horror Story";
+    } else if (chosenCategory === categories[2]) {
+      catagoryName.innerHTML = "The Chosen Category Is Beyoncé";
+    }
   }
 
-  // If we are out of guesses...
-  if (guessesLeft === 0) {
+  // Create guesses ul
+   result = function () {
+    wordHolder = document.getElementById('hold');
+    correct = document.createElement('ul');
 
-    // Then we will loss and we'll update the HTML to display the loss.
-    losses++;
-    document.querySelector("#losses").innerHTML = losses;
+    for (var i = 0; i < word.length; i++) {
+      correct.setAttribute('id', 'my-word');
+      guess = document.createElement('li');
+      guess.setAttribute('class', 'guess');
+      if (word[i] === "-") {
+        guess.innerHTML = "-";
+        space = 1;
+      } else {
+        guess.innerHTML = "_";
+      }
 
-    // Then we'll call the reset.
-    reset();
+      guesses.push(guess);
+      wordHolder.appendChild(correct);
+      correct.appendChild(guess);
+    }
   }
-};
+  
+  // Show lives
+   comments = function () {
+    showLives.innerHTML = "You have " + lives + " lives";
+    if (lives < 1) {
+      showLives.innerHTML = "Game Over";
+    }
+    for (var i = 0; i < guesses.length; i++) {
+      if (counter + space === guesses.length) {
+        showLives.innerHTML = "You Win!";
+      }
+    }
+  }
+
+  //     // Animate man
+  // var animate = function () {
+  //   var drawMe = lives ;
+  //   drawArray[drawMe]();
+  // }
+
+  
+//    // Hangman
+//   canvas =  function(){
+
+//     myStickman = document.getElementById("stickman");
+//     context = myStickman.getContext('2d');
+//     context.beginPath();
+//     context.strokeStyle = "#fff";
+//     context.lineWidth = 2;
+//   };
+  
+//     head = function(){
+//       myStickman = document.getElementById("stickman");
+//       context = myStickman.getContext('2d');
+//       context.beginPath();
+//       context.arc(60, 25, 10, 0, Math.PI*2, true);
+//       context.stroke();
+//     }
+    
+//   draw = function($pathFromx, $pathFromy, $pathTox, $pathToy) {
+    
+//     context.moveTo($pathFromx, $pathFromy);
+//     context.lineTo($pathTox, $pathToy);
+//     context.stroke(); 
+// }
+
+//    frame1 = function() {
+//      draw (0, 150, 150, 150);
+//    };
+   
+//    frame2 = function() {
+//      draw (10, 0, 10, 600);
+//    };
+  
+//    frame3 = function() {
+//      draw (0, 5, 70, 5);
+//    };
+  
+//    frame4 = function() {
+//      draw (60, 5, 60, 15);
+//    };
+  
+//    torso = function() {
+//      draw (60, 36, 60, 70);
+//    };
+  
+//    rightArm = function() {
+//      draw (60, 46, 100, 50);
+//    };
+  
+//    leftArm = function() {
+//      draw (60, 46, 20, 50);
+//    };
+  
+//    rightLeg = function() {
+//      draw (60, 70, 100, 100);
+//    };
+  
+//    leftLeg = function() {
+//      draw (60, 70, 20, 100);
+//    };
+  
+//   drawArray = [rightLeg, leftLeg, rightArm, leftArm,  torso,  head, frame4, frame3, frame2, frame1]; 
+
+
+  // OnClick Function
+   check = function () {
+    list.onclick = function () {
+      var guess = (this.innerHTML);
+      this.setAttribute("class", "active");
+      this.onclick = null;
+      for (var i = 0; i < word.length; i++) {
+        if (word[i] === guess) {
+          geusses[i].innerHTML = guess;
+          counter += 1;
+        } 
+      }
+      var j = (word.indexOf(guess));
+      if (j === -1) {
+        lives -= 1;
+        comments();
+        animate();
+      } else {
+        comments();
+      }
+    }
+  }
+  
+    
+  // Play
+  play = function () {
+    categories = [
+        ["whitley", "dwyane", "kimberly", "ron", "denise", "hillman", "virginia"],
+        ["tate", "aslym", "coven", "balenciaga", "roanoke"],
+        ["houston", "blue", "lemonade", "bey-hive", "four"]
+    ];
+
+    chosenCategory = categories[Math.floor(Math.random() * categories.length)];
+    word = chosenCategory[Math.floor(Math.random() * chosenCategory.length)];
+    word = word.replace(/\s/g, "-");
+    console.log(word);
+    buttons();
+
+    guesses = [ ];
+    lives = 10;
+    counter = 0;
+    space = 0;
+    result();
+    comments();
+    selectCat();
+    canvas();
+  }
+
+  play();
+  
+  // Hint
+
+    hint.onclick = function() {
+
+      hints = [
+        ["Last name gilbert", "Originally from brooklyn", "Future Doctor", "Married Freddie", "Name of Ficitonal University", "Located in This State"],
+        ["Latex Man", "Season Two Theme", "Witches", "Famous Last Words", "Colonial Mystery"],
+        ["Home Town", "Name of first child", "Recent solo album", "Name of her stans", "Favorite Number"]
+    ];
+
+    var catagoryIndex = categories.indexOf(chosenCategory);
+    var hintIndex = chosenCategory.indexOf(word);
+    showClue.innerHTML = "Clue: - " +  hints [catagoryIndex][hintIndex];
+  };
+
+   // Reset
+
+  document.getElementById('reset').onclick = function() {
+    correct.parentNode.removeChild(correct);
+    letters.parentNode.removeChild(letters);
+    showClue.innerHTML = "";
+    context.clearRect(0, 0, 400, 400);
+    play();
+  }
+}
+
+
